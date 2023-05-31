fb7156f19ce7ec7296a77449b50bbdc473f3737f
beverley-dick <beverleydick11@gmail.com>
2023-05-31 09:39:30 +0300

    Add project files.

---
+++ static/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace staticConstuctor
+{
+    class Car
+{
+        //static constructor
+        static Car ()
+        {
+            Console.WriteLine("Static Constructor"); 
+        }
+
+        //parameterless constructor
+        Car()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+    static void Main(string[] args)
+    {
+            //call parameterless constructor
+            Car car1 = new Car();
+
+            //call parameterless constructor again
+            Car car2 = new Car();
+
+            Console.ReadLine();
+    }
+}
+}
